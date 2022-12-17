1a456d4420e14ac86fe74cf67a4f46a12d42b870
darshansgowda17 <your_email_address@example.com>
2022-12-17 20:44:48 +0530

    added files form upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,11 @@
+var numberOfDrums = document.querySelectorAll(".drum").length
+for(var i = 0;i < numberOfDrums;i ++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        
+    })
+}
+
+
+
